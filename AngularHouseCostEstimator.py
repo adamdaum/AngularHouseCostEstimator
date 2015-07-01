c6314378b95ea2ebe3efce0b9a9ab93162ab5dbc
adamdaum <daumadam@gmail.com>
2015-07-01 16:12:45 -0700

    Initial project commit.

---
+++ AngularHouseCostEstimator.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask.ext.restless import APIManager
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, Text, Float, Date
+
+app = Flask(__name__, static_url_path='')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://adamdaum:t7w94u3qA@localhost/projects'
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, unique=False)
+    description = Column(Text, unique=False)
+    vendor = Column(Text, unique=False)
+    hours = Column(Float, unique=False)
+    cost = Column(Float, unique=False)
+    start = Column(Date, unique=False)
+    finish = Column(Date, unique=False)
+
+
+db.create_all()
+
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
+api_manager.create_api(Project, methods=['GET', 'POST', 'DELETE', 'PUT'])
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+
+app.debug = True
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/estimator.js
@@ -0,0 +1,47 @@
+var myApp = angular.module('myApp',[]);
+
+ myApp.controller('myCtrl', function($scope, $http){
+     $http.get("/api/project").success(function(data){
+        $scope.projects = data.objects;
+     });
+
+     $scope.addProject = function(){
+        $http.post("/api/project", {
+            name: $scope.inputName,
+            description: $scope.inputDescription,
+            vendor: $scope.inputVendor,
+            hours: $scope.inputHours,
+            cost: $scope.inputCost,
+            start: $scope.inputStart,
+            finish: $scope.inputFinish,
+        }).success(function(data){
+            $scope.projects.push(data);
+
+        });
+         $scope.inputName = '';
+         $scope.inputDescription = '';
+         $scope.inputVendor = '';
+         $scope.inputHours = '';
+         $scope.inputCost = '';
+         $scope.inputStart = '';
+         $scope.inputFinish = '';
+     };
+
+     $scope.deleteProject = function (project){
+        $http.delete("/api/project/" + project.id).success(function(response) {
+            $scope.projects.splice($scope.projects.indexOf(project), 1)
+        })
+    };
+
+     $scope.getTotal = function(){
+       var total = 0;
+         for(var i=0; i < $scope.projects.length; i++){
+             var project = $scope.projects[i];
+             total += project.cost;
+         }
+         return total
+     };
+
+ });
+
+
